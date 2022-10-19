1338d2c4d145b0ad1667c7a4bdde35cb1fb975f3
Nikhil Badyal <nikhill773384@gmail.com>
2022-10-19 19:05:13 +0530

    📝 Updated envs [skip ci]

--- export_env.sh
+++ export_env.sh
@@ -7,7 +7,6 @@ set -o nounset
 export PATCH_APPS=youtube,twitter,reddit,youtube_music
 export BUILD_EXTENDED=True
 export EXCLUDE_PATCH_YOUTUBE=custom-branding,enable-debugging
-export EXCLUDE_PATCH_YOUTUBE_EXTENDED=custom-branding-icon-red,custom-branding-icon-blue,custom-branding-revancify,custom-branding-name,custom-package-name,materialyou,always-autorepeat,overlay-buttons-alternative-icon,enable-wide-searchbar,tablet-mini-player
-export EXCLUDE_PATCH_YOUTUBE_MUSIC_EXTENDED=custom-branding-music,custom-branding-music-decipher3114,custom-package-name-music,custom-branding-music-red
-export BUILD_OG_BRANDING_YOUTUBE=True
-export BRANDING_PATCH=custom-branding-icon-blue
+export EXCLUDE_PATCH_YOUTUBE_EXTENDED=custom-branding-icon-red,custom-branding-icon-blue,custom-branding-icon-revancify,custom-branding-name,custom-package-name,materialyou
+export EXCLUDE_PATCH_YOUTUBE_MUSIC_EXTENDED=custom-branding-music-red,custom-branding-music-revancify,custom-package-name-music,compact-header
+export ARCHS_TO_BUILD=arm64-v8a
