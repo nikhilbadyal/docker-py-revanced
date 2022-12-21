a60330b408f65ffc0a4c0854f513030d0dc3639b
Nikhil Badyal <nikhill773384@gmail.com>
2022-12-21 18:22:41 +0530

    👷 Test all apps in PR checks

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,26 @@
 from typing import Dict
 
 default_build = [
+    "ticktick",
+]
+all_builds = [
     "youtube",
+    "youtube_music",
+    "twitter",
+    "reddit",
+    "tiktok",
+    "warnwetter",
+    "spotify",
+    "nyx-music-player",
+    "icon_pack_studio",
+    "ticktick",
+    "twitch",
+    "hex-editor",
+    "citra",
+    "windy",
+    "my-expenses",
+    "backdrops",
+    "expensemanager",
 ]
 possible_archs = ["armeabi-v7a", "x86", "x86_64", "arm64-v8a"]
 
