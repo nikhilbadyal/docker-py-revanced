936f392897708f8c304262412bdebb8f8279d8f7
Nikhil Badyal <nikhill773384@gmail.com>
2022-12-29 12:32:14 +0530

    📝 Updated sample ENVS [skip ci]

--- export_env.sh
+++ export_env.sh
@@ -4,9 +4,10 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-export PATCH_APPS=youtube,twitter,reddit,youtube_music
+export PATCH_APPS=youtube,youtube_music
 export BUILD_EXTENDED=True
-export EXCLUDE_PATCH_YOUTUBE=custom-branding,enable-debugging
-export EXCLUDE_PATCH_YOUTUBE_EXTENDED=custom-branding-icon-red,custom-branding-icon-blue,custom-branding-icon-revancify,custom-branding-name,custom-package-name,materialyou,overlay-buttons-alternative-icon
-export EXCLUDE_PATCH_YOUTUBE_MUSIC_EXTENDED=custom-branding-music-red,custom-branding-music-revancify,custom-package-name-music,compact-header
+export EXCLUDE_PATCH_YOUTUBE=custom-branding,debugging
+export EXCLUDE_PATCH_TWITCH=debug-mode
+export EXCLUDE_PATCH_YOUTUBE_EXTENDED=custom-branding-icon-afn-red,custom-branding-icon-afn-blue,custom-branding-icon-revancify,custom-branding-name
+export EXCLUDE_PATCH_YOUTUBE_MUSIC_EXTENDED=custom-branding-music-red,custom-branding-music-revancify,compact-header
 export ARCHS_TO_BUILD=arm64-v8a
