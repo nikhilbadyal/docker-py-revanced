73bdda9c821ba8ba7c6421efcbaa30755057bb1d
tenyuhuang <16178657+tenyuhuang@users.noreply.github.com>
2023-07-05 10:55:46 +0800

    Fix Revanced Extended App IDs

--- src/patches.py
+++ src/patches.py
@@ -48,7 +48,7 @@ class Patches(object):
     }
     _revanced_extended_app_ids = {
         "com.google.android.youtube": ("youtube", "_yt"),
-        "com.google.android.apps.youtube.music": ("youtube-music", "_ytm"),
+        "com.google.android.apps.youtube.music": ("youtube_music", "_ytm"),
     }
     revanced_extended_app_ids = {
         key: (value[0], "_" + value[0])
