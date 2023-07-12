27311d37093b8b00c62cb22ffa44fc3f9f51fe9b
Spacellary <39604793+Spacellary@users.noreply.github.com>
2023-07-12 05:20:05 -0300

    Update patches.py

--- src/patches.py
+++ src/patches.py
@@ -50,12 +50,10 @@ class Patches(object):
     _revanced_extended_app_ids = {
         "com.google.android.youtube": ("youtube", "_yt"),
         "com.google.android.apps.youtube.music": ("youtube_music", "_ytm"),
-        "com.reddit.frontpage": ("reddit", "_reddit"),
     }
     revanced_extended_app_ids = {
         key: (value[0], "_" + value[0])
         for key, value in _revanced_extended_app_ids.items()
-        
     }
 
     @staticmethod
