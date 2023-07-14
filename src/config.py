f0fd98af0fce0b386c4e4334d3dd35d57c28e475
Nikhil Badyal <nikhill773384@gmail.com>
2023-07-14 08:48:38 +0530

    🐛 Removed space from app config

--- src/config.py
+++ src/config.py
@@ -82,9 +82,9 @@ class RevancedConfig(object):
             "boost": f"{self.apk_mirror}/apk/ruben-mayayo/boost-for-reddit/",
             "rif": f"{self.apk_mirror}/apk/talklittle/reddit-is-fun/",
             "sync": f"{self.apk_mirror}/apk/red-apps-ltd/sync-for-reddit/",
-            "infinity ": f"{self.apk_mirror}/apk/docile-alligator/infinity-for-reddit/",
-            "slide ": f"{self.apk_mirror}/apk/haptic-apps/slide-for-reddit/",
-            "bacon ": f"{self.apk_mirror}/apk/onelouder-apps/baconreader-for-reddit/",
+            "infinity": f"{self.apk_mirror}/apk/docile-alligator/infinity-for-reddit/",
+            "slide": f"{self.apk_mirror}/apk/haptic-apps/slide-for-reddit/",
+            "bacon": f"{self.apk_mirror}/apk/onelouder-apps/baconreader-for-reddit/",
         }
         self.apk_mirror_version_urls = {
             key: value + value.split("/")[-2]
