ad80cc888ae2ac28c09c2e19d181c05dd9521fc0
IMXEren <96839938+IMXEren@users.noreply.github.com>
2023-07-14 17:52:56 +0530

    Update patches.py

--- src/patches.py
+++ src/patches.py
@@ -57,6 +57,7 @@ class Patches(object):
     _revanced_extended_app_ids = {
         "com.google.android.youtube": "youtube",
         "com.google.android.apps.youtube.music": "youtube_music",
+        "com.mgoogle.android.gms": "MicroG"
     }
     revanced_extended_app_ids = {
         key: (value, "_" + value) for key, value in _revanced_extended_app_ids.items()
