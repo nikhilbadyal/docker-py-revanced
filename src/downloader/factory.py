6c0da4e45bd5ad30c28d534484af2f6d5f18f326
Nikhil Badyal <59223300+nikhilbadyal@users.noreply.github.com>
2023-08-31 11:46:23 +0530

    ✨ Added google drive downloader (#359)

--- src/downloader/factory.py
+++ src/downloader/factory.py
@@ -6,11 +6,13 @@ from src.downloader.apkpure import ApkPure
 from src.downloader.apksos import ApkSos
 from src.downloader.download import Downloader
 from src.downloader.github import Github
+from src.downloader.google_drive import GoogleDrive
 from src.downloader.sources import (
     APK_MIRROR_BASE_URL,
     APK_MONK_BASE_URL,
     APK_PURE_BASE_URL,
     APKS_SOS_BASE_URL,
+    DRIVE_DOWNLOAD_BASE_URL,
     GITHUB_BASE_URL,
 )
 from src.downloader.uptodown import UptoDown
@@ -41,5 +43,7 @@ class DownloaderFactory(object):
             return ApkMirror(config)
         if apk_source.startswith(APK_MONK_BASE_URL):
             return ApkMonk(config)
+        if apk_source.startswith(DRIVE_DOWNLOAD_BASE_URL):
+            return GoogleDrive(config)
         msg = "No download factory found."
-        raise DownloadError(msg)
+        raise DownloadError(msg, url=apk_source)
