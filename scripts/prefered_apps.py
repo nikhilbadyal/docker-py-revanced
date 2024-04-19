ebe7a9422a877130161dc8b5c65aaf1027e12b97
Nikhil Badyal <nikhill773384@gmail.com>
2024-04-20 00:05:48 +0530

    🐛 Handle default patch apps

--- scripts/prefered_apps.py
+++ scripts/prefered_apps.py
@@ -5,12 +5,14 @@ import os
 import dotenv
 from loguru import logger
 
+from src.utils import default_build
+
 
 def update_patch_apps() -> None:
     """Update preferred apps."""
     dotenv_file = dotenv.find_dotenv()
     dotenv.load_dotenv(dotenv_file)
-    patch_apps = os.environ["PATCH_APPS"]
+    patch_apps = os.environ.get("PATCH_APPS", default_build)
     logger.info(f"PATCH_APPS is currently {patch_apps}")
     os.environ["PATCH_APPS"] = os.environ["PREFERRED_PATCH_APPS"]
     new_patch_apps = os.environ["PATCH_APPS"]
