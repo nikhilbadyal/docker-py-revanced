614046a15aa630ac0718659a61a1c461a7e1a0e0
Nikhil Badyal <nikhill773384@gmail.com>
2024-11-12 22:00:35 +0530

    🐛 Updated for revanced cli v5

--- check_resource_updates.py
+++ check_resource_updates.py
@@ -6,7 +6,7 @@ from loguru import logger
 from main import get_app
 from src.config import RevancedConfig
 from src.manager.github import GitHubManager
-from src.utils import default_build, integration_version_key, integrations_dl_key, patches_dl_key, patches_version_key
+from src.utils import default_build, patches_dl_key, patches_version_key
 
 
 def check_if_build_is_required() -> bool:
@@ -18,17 +18,10 @@ def check_if_build_is_required() -> bool:
     for app_name in env.list("PATCH_APPS", default_build):
         logger.info(f"Checking {app_name}")
         app_obj = get_app(config, app_name)
-        old_integration_version = GitHubManager(env).get_last_version(app_obj, integration_version_key)
-        old_integration_source = GitHubManager(env).get_last_version_source(app_obj, integrations_dl_key)
         old_patches_version = GitHubManager(env).get_last_version(app_obj, patches_version_key)
         old_patches_source = GitHubManager(env).get_last_version_source(app_obj, patches_dl_key)
         app_obj.download_patch_resources(config)
         if GitHubManager(env).should_trigger_build(
-            old_integration_version,
-            old_integration_source,
-            app_obj.resource["integrations"]["version"],
-            app_obj.integrations_dl,
-        ) or GitHubManager(env).should_trigger_build(
             old_patches_version,
             old_patches_source,
             app_obj.resource["patches"]["version"],
@@ -36,10 +29,6 @@ def check_if_build_is_required() -> bool:
         ):
             caused_by = {
                 "app_name": app_name,
-                "integration": {
-                    "old": old_integration_version,
-                    "new": app_obj.resource["integrations"]["version"],
-                },
                 "patches": {
                     "old": old_patches_version,
                     "new": app_obj.resource["patches"]["version"],
