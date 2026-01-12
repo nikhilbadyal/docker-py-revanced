b7e2d2b83871c3bfba08e3ea0b4b15bbb4fa5667
IMXEren <96839938+IMXEren@users.noreply.github.com>
2026-01-12 19:25:43 +0530

    feat(apps): add samsung-radio and rakuten-viber

--- src/patches.py
+++ src/patches.py
@@ -89,8 +89,10 @@ class Patches(object):
         "com.nousguide.android.orftvthek": "orf-on",
         "com.pandora.android": "pandora",
         "it.ipzs.cieid": "cieid",
-        "ml.docilealligator.infinityforreddit.patreon": "infinity-for-reddit-patreon",
-        "ml.docilealligator.infinityforreddit.plus": "infinity-for-reddit-plus",
+        "ml.docilealligator.infinityforreddit.patreon": "infinity-patreon",
+        "ml.docilealligator.infinityforreddit.plus": "infinity-plus",
+        "com.sec.android.app.fm": "samsung-radio",
+        "com.viber.voip": "rakuten-viber",
     }
 
     @staticmethod
