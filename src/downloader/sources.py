47bc3883b6e0be497edaad74cbaccc78801a24fb
IMXEren <96839938+IMXEren@users.noreply.github.com>
2026-01-12 19:40:01 +0530

    style: fix formatting

--- src/downloader/sources.py
+++ src/downloader/sources.py
@@ -31,7 +31,10 @@ apk_sources = {
     "grecorder": f"{APK_MIRROR_BASE_APK_URL}/google-inc/google-recorder/google-recorder",
     "icon_pack_studio": f"{APK_MIRROR_BASE_APK_URL}/smart-launcher-team/icon-pack-studio/icon-pack-studio",
     "infinity": f"{APK_MIRROR_BASE_APK_URL}/docile-alligator/infinity-for-reddit/infinity-for-reddit",
-    "inshorts": f"{APK_MIRROR_BASE_APK_URL}/inshorts-formerly-news-in-shorts/inshorts-news-in-60-words-2/inshorts-news-in-60-words",
+    "inshorts": (
+        f"{APK_MIRROR_BASE_APK_URL}/inshorts-formerly-news-in-shorts/"
+        "inshorts-news-in-60-words-2/inshorts-news-in-60-words"
+    ),
     "instagram": f"{APK_MIRROR_BASE_APK_URL}/instagram/instagram-instagram/instagram",
     "irplus": f"{APK_MIRROR_BASE_APK_URL}/binarymode/irplus-infrared-remote/irplus-infrared-remote",
     "lightroom": f"{APK_MIRROR_BASE_APK_URL}/adobe/lightroom/lightroom-photo-video-editor",
@@ -49,23 +52,32 @@ apk_sources = {
     "sonyheadphone": f"{APK_MIRROR_BASE_APK_URL}/sony-corporation/sony-headphones-connect/sony-sound-connect",
     "sync": f"{APK_MIRROR_BASE_APK_URL}/red-apps-ltd/sync-for-reddit/sync-for-reddit",
     "tasker": f"{APK_MIRROR_BASE_APK_URL}/joaomgcd/tasker-crafty-apps-eu/tasker-crafty-apps-eu",
-    "ticktick": f"{APK_MIRROR_BASE_APK_URL}/appest-inc/ticktick-to-do-list-with-reminder-day-planner/ticktickto-do-list-calendar",
+    "ticktick": (
+        f"{APK_MIRROR_BASE_APK_URL}/appest-inc/ticktick-to-do-list-with-reminder-day-planner/"
+        "ticktickto-do-list-calendar"
+    ),
     "tiktok": f"{APK_MIRROR_BASE_APK_URL}/tiktok-pte-ltd/tik-tok/tiktok",
     "trakt": f"{APK_MIRROR_BASE_APK_URL}/trakt/trakt/trakt-tv-shows-movies",
     "twitch": f"{APK_MIRROR_BASE_APK_URL}/twitch-interactive-inc/twitch/twitch-live-streaming",
     "twitter": f"{APK_MIRROR_BASE_APK_URL}/x-corp/twitter/x",
     "vsco": f"{APK_MIRROR_BASE_APK_URL}/vsco/vsco-photo-video-editor-3/vsco-photo-editor",
     "warnwetter": f"{APK_MIRROR_BASE_APK_URL}/deutscher-wetterdienst/warnwetter/warnwetter",
-    "windy": f"{APK_MIRROR_BASE_APK_URL}/windy-weather-world-inc/windy-wind-weather-forecast/windy-app-enhanced-forecast",
+    "windy": (
+        f"{APK_MIRROR_BASE_APK_URL}/windy-weather-world-inc/windy-wind-weather-forecast/windy-app-enhanced-forecast"
+    ),
     "youtube": f"{APK_MIRROR_BASE_APK_URL}/google-inc/youtube/youtube",
     "youtube_music": f"{APK_MIRROR_BASE_APK_URL}/google-inc/youtube-music/youtube-music",
     "yuka": f"{APK_MIRROR_BASE_APK_URL}/yuka-apps/yuka-food-cosmetic-scan/yuka-food-cosmetic-scanner",
     "strava": f"{APK_MIRROR_BASE_APK_URL}/strava-inc/strava-run-bike-hike-2/strava-run-bike-hike",
     "tumblr": f"{APK_MIRROR_BASE_APK_URL}/tumblr-inc/tumblr/tumblr-social-media-art",
-    "fitnesspal": f"{APK_MIRROR_BASE_APK_URL}/myfitnesspal-inc/calorie-counter-myfitnesspal/myfitnesspal-calorie-counter",
+    "fitnesspal": (
+        f"{APK_MIRROR_BASE_APK_URL}/myfitnesspal-inc/calorie-counter-myfitnesspal/myfitnesspal-calorie-counter"
+    ),
     "facebook": f"{APK_MIRROR_BASE_APK_URL}/facebook-2/facebook/facebook",
     "lemmy-sync": f"{APK_MIRROR_BASE_APK_URL}/sync-apps-ltd/sync-for-lemmy/sync-for-lemmy",
-    "xiaomi-wearable": f"{APK_MIRROR_BASE_APK_URL}/beijing-xiaomi-mobile-software-co-ltd/mi-wear-小米穿戴/mi-fitness-xiaomi-wear",
+    "xiaomi-wearable": (
+        f"{APK_MIRROR_BASE_APK_URL}/beijing-xiaomi-mobile-software-co-ltd/mi-wear-小米穿戴/mi-fitness-xiaomi-wear"
+    ),
     "my-expenses": UPTODOWN_BASE_URL.format("my-expenses"),
     "spotify": UPTODOWN_BASE_URL.format("spotify"),
     "joey": UPTODOWN_BASE_URL.format("joey-for-reddit"),
@@ -86,15 +98,23 @@ apk_sources = {
     "soundcloud": f"{APK_MIRROR_BASE_APK_URL}/soundcloud/soundcloud-soundcloud/soundcloud-the-music-you-love",
     "stocard": f"{APK_MIRROR_BASE_APK_URL}/stocard-gmbh/stocard-rewards-cards-wallet/stocard-rewards-cards-wallet",
     "willhaben": f"{APK_MIRROR_BASE_APK_URL}/willhaben/willhaben/willhaben",
-    "proton-mail": f"{APK_MIRROR_BASE_APK_URL}/proton-technologies-ag/protonmail-encrypted-email/proton-mail-encrypted-email",
+    "proton-mail": (
+        f"{APK_MIRROR_BASE_APK_URL}/proton-technologies-ag/protonmail-encrypted-email/proton-mail-encrypted-email"
+    ),
     "prime-video": f"{APK_MIRROR_BASE_APK_URL}/amazon-mobile-llc/amazon-prime-video/amazon-prime-video",
-    "cricbuzz": f"{APK_MIRROR_BASE_APK_URL}/cricbuzz-com/cricbuzz-live-cricket-scores-news/cricbuzz-live-cricket-scores",
+    "cricbuzz": (
+        f"{APK_MIRROR_BASE_APK_URL}/cricbuzz-com/cricbuzz-live-cricket-scores-news/cricbuzz-live-cricket-scores"
+    ),
     "crunchyroll": f"{APK_MIRROR_BASE_APK_URL}/crunchyroll-llc-2/crunchyroll/crunchyroll",
     "threads": f"{APK_MIRROR_BASE_APK_URL}/instagram/threads-an-instagram-app/threads",
-    "orf-on": f"{APK_MIRROR_BASE_APK_URL}/orf-osterreichischer-rundfunk/orf-tvthek-video-on-demand-android-tv/orf-on-android-tv",
+    "orf-on": (
+        f"{APK_MIRROR_BASE_APK_URL}/orf-osterreichischer-rundfunk/orf-tvthek-video-on-demand-android-tv/orf-on-android-tv"
+    ),
     "pandora": f"{APK_MIRROR_BASE_APK_URL}/pandora/pandora-music-podcasts/pandora-music-podcasts",
     "cieid": f"{APK_MIRROR_BASE_APK_URL}/istituto-poligrafico-e-zecca-dello-stato-s-p-a/cieid/cieid",
-    "infinity-patreon": f"{APK_MIRROR_BASE_APK_URL}/docile-alligator/infinity-for-reddit-patreon-github-version/infinity-for-reddit-patreon-github-version",
+    "infinity-patreon": (
+        f"{APK_MIRROR_BASE_APK_URL}/docile-alligator/infinity-for-reddit-patreon-github-version/infinity-for-reddit-patreon-github-version"
+    ),
     "infinity-plus": f"{APK_MIRROR_BASE_APK_URL}/docile-alligator/infinity-for-reddit-plus/infinity-for-reddit",
     "samsung-radio": f"{APK_MIRROR_BASE_APK_URL}/samsung-electronics-co-ltd/samsung-radio/samsung-radio",
     "rakuten-viber": f"{APK_MIRROR_BASE_APK_URL}/viber-media-s-a-r-l/viber/rakuten-viber-messenger",
